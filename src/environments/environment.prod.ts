661eff5691b1d260dfd64081a7783e6f9f410362
JDiaz <js.diazn@gmail.com>
2021-01-05 12:40:09 -0300

    l

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@
  */
 export const environment = {
   production: true,
-  apiUrl: 'http://161.35.6.208:3000/api',
+  apiUrl: 'https://ph-back.herokuapp.com/',
   // apiUrl: 'http://192.168.0.234:3000/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,5 +10,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://ph-back.herokuapp.com/'
 };
